3645bdccbf22a6274296fcda2a2ea03ba864ba56
Paper-todd <debt4000@gmail.com>
2026-04-24 06:41:28 +0000

    d

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Rattasorn"
+last_name = "Mahadiloke"
+print(first_name , last_name)
---
+++ cell01/ex01/name2.py
@@ -0,0 +1,4 @@
+first_name = "Rattasorn"
+last_name = "Mahadiloke"
+whole_name = first_name + " " + last_name
+print(whole_name)
